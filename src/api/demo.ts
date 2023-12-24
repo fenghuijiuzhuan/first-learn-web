5c72f700f6ee53c0eae7655c652ffa6e8845c6d0
Yvan <fenghuijiuzhuan@163.com>
2023-12-24 22:19:10 +0800

    feat: http/https数据传输5种方式-url param,query,form urlencoded,json,form data

--- src/api/demo.ts
+++ src/api/demo.ts
@@ -1,3 +1,4 @@
+import { ContentTypeEnum } from '@/enums/httpEnum'
 import { defHttp } from '@/utils/http/axios'
 
 export const getPersonById = (params) => {
@@ -6,4 +7,28 @@ export const getPersonById = (params) => {
 
 export const queryPerson = (params) => {
   return defHttp.get({ url: '/person/find', params })
+}
+
+export const formUrl = (params) => {
+  return defHttp.post({
+    url: '/person',
+    data: params,
+    headers: {
+      'content-type': ContentTypeEnum.FORM_URLENCODED
+    }
+  })
+}
+
+export const json = (params) => {
+  return defHttp.post({ url: '/person', data: params })
+}
+
+export const formData = (params) => {
+  return defHttp.post({
+    url: '/person/file',
+    data: params,
+    headers: {
+      'Content-Type': ContentTypeEnum.FORM_DATA
+    }
+  })
 }
--- src/views/HelloWorld/index.vue
+++ src/views/HelloWorld/index.vue
@@ -19,30 +19,63 @@
     <Col :span="6" />
     <Col :span="6" />
     <Col :span="6" />
-    <Col :span="6" />
+    <Col :span="6">
+      <input ref="fileInput" type="file" />
+    </Col>
   </Row>
 </template>
 
 <script setup lang="ts">
   import { Col, Row } from 'ant-design-vue'
-  import { getPersonById, queryPerson } from '../../api/demo'
-  import { onMounted } from 'vue'
+  import { getPersonById, queryPerson, formUrl, json, formData } from '../../api/demo'
+  import { onMounted, ref } from 'vue'
 
-  async function getPerson() {
+  async function sendUrlParam() {
     const res = await getPersonById('/123')
     console.log(res)
   }
 
-  async function queryPersonByPerson() {
+  async function sendQuery() {
     const res = await queryPerson({
-      name: '嘎嘎嘎',
+      name: 'query',
       age: 21
     })
     console.log(res)
   }
 
+  async function sendFormUrlEncode() {
+    const res = await formUrl({
+      name: 'formUrlEncode',
+      age: 18
+    })
+    console.log(res)
+  }
+
+  async function sendJson() {
+    const res = await json({
+      name: 'json',
+      age: 19
+    })
+    console.log(res)
+  }
+
+  const fileInput = ref()
+  async function sendformData() {
+    const data = new FormData()
+    data.set('name', 'form-data')
+    data.set('age', '20')
+    data.set('file1', fileInput.value.files[0])
+    data.set('file2', fileInput.value.files[1])
+
+    const res = await formData(data)
+    console.log(res)
+  }
+
   onMounted(() => {
-    getPerson()
-    queryPersonByPerson()
+    sendUrlParam()
+    sendQuery()
+    sendFormUrlEncode()
+    sendJson()
+    fileInput.value.onchange = sendformData
   })
 </script>
